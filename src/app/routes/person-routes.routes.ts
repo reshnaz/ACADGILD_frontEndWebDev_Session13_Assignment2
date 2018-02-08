964d6dbfdf1079417d91276299aa333e09b119b6
Reshma Hussain <reshnaz91@gmail.com>
2018-02-08 19:41:26 +0530

    Commit

--- src/app/routes/person-routes.routes.ts
+++ src/app/routes/person-routes.routes.ts
@@ -3,13 +3,16 @@ import { AddPersonComponent } from './../add-person/add-person.component';
 import { PersonComponent } from '../person/person.component';
 import { PageNotFoundComponent } from './../page-not-found/page-not-found.component'
 import { PersonDetailComponent } from '../person-detail/person-detail.component';
+import { LoginComponent } from '../login/login.component';
+import { Guard } from '../routes/guard';
 /*
 Here we configure routing and router outlet.
 Each path is mapped to corresponding component it needs to navigate to.
 */
 const routes: Routes = [
     { path: '', redirectTo: '/addPerson', pathMatch: 'full' },
-    { path: 'addPerson', component: AddPersonComponent },
+    { path: 'login', component: LoginComponent},
+    { path: 'addPerson', component: AddPersonComponent, canActivate: [Guard] },
     { path: 'personList', component: PersonComponent },
     { path: 'personDetail/:id', component: PersonDetailComponent },
     { path: '**', component: PageNotFoundComponent }
